da709d9edd7eb8d9f6d3d7b58183b7215eaceb53
Temesgen Tesfamichael <natitesfazgi@gmail.com>
2024-01-15 22:10:22 -0800

    Changes

---
+++ input_generator.py
@@ -0,0 +1,66 @@
+import random
+
+
+class InputGenerator:
+    def __init__(self, output_file):
+        self.output_file = output_file
+        self.output = ""
+
+    def get_output(self):
+        return self.output
+
+    def input_generator(self):
+        row = int(input("How many rows do you want the field to have? "))
+        col = int(input("How many columns do you want the field to have? "))
+        chance = float(input("Give the probability of the mine in the field (a decimal number): "))
+        item_chances = [chance, 1 - chance]
+        items = ["mine", "no_mine"]
+        field_line = ""
+        for i in range(row):
+            field_col = ""
+            for j in range(col):
+                mine_success = random.choices(items, item_chances)
+                if mine_success == ["mine"]:
+                    field_col += "*"
+                else:
+                    field_col += "."
+            field_line += f"{field_col}\n"
+
+        self.output += f"{row} {col}\n"
+        self.output += field_line
+
+    def random_generator(self):
+        row = random.randint(1, 100)
+        col = random.randint(1, 100)
+        field_line = ""
+        for i in range(row):
+            field_col = ""
+            for j in range(col):
+                if random.choice([True, False]):
+                    field_col += "*"
+                else:
+                    field_col += "."
+            field_line += f"{field_col}\n"
+
+        self.output += f"{row} {col}\n"
+        self.output += field_line
+
+    def mine_fields_generator(self, n):
+        for i in range(n):
+            self.random_generator()
+
+    def ending_input(self):
+        self.output += f"0 0\n"
+
+    def write_output(self):
+        with open(self.output_file, "w", encoding='utf-8') as f:
+            f.write(self.output)
+
+
+if __name__ == "__main__":
+    generator = InputGenerator("minesweeper_input.txt")
+    # generator.input_generator()
+    generator.mine_fields_generator(3)
+    generator.ending_input()
+    generator.write_output()
+    print(generator.output)
---
+++ minesweeper.py
@@ -0,0 +1,172 @@
+class MineSweeper:
+    def __init__(self, input_file, output_file):
+        self.input_file = input_file
+        self.output_file = output_file
+        self.output = ""
+        self.field_count = 0
+        self.rows = 0
+        self.cols = 0
+
+    def get_output(self):
+        """
+        :return: the output
+        """
+        return self.output
+
+    def write_output(self):
+        """
+        Writes the output produced to output file created.
+        """
+        with open(self.output_file, "w", encoding='utf-8') as f:
+            f.write(self.output)
+
+    def read_input(self):
+        """
+        reads the input provided from an input file provided and writes the output
+        by calling other function.
+        """
+        file_name = open(self.input_file, "r")
+        while file_name:
+            first_line = file_name.readline()
+            if first_line == "":
+                break
+            self.rows, self.cols = (int(val) for val in first_line.split())
+            if self.rows == 0 and self.cols == 0:
+                break
+            if isinstance(self.rows, int) and isinstance(self.cols, int):
+                self.field_count += 1
+                self.output += f"Field #{self.field_count} :\n"
+
+            single_field = []
+            for row in range(self.rows):
+                line = file_name.readline().strip()
+                field_line = [*line]
+                single_field.append(field_line)
+            self.check_mine(single_field)
+        file_name.close()
+
+    def check_mine(self, single_field):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if single_field[i][j] == "*":
+                    continue
+                adjacent = 0
+                if single_field[i][j] == ".":
+                    if len(single_field[i]) == 1 and len(single_field) == 1:  # in case of single row & col
+                        single_field[i][j] == 0
+                    elif len(single_field[i]) == 1 and len(single_field) > 1:  # in case of one col
+                        if i != len(single_field) - 1:
+                            if single_field[i + 1][j] == "*":
+                                adjacent += 1
+                        else:
+                            if single_field[i - 1][j] == "*":
+                                adjacent += 1
+                    elif len(single_field) == 1 and len(single_field[i]) > 1:  # case of one row
+                        if j != len(single_field[i]) - 1:
+                            if single_field[i][j + 1] == "*":
+                                adjacent += 1
+                            else:
+                                if single_field[i][j - 1] == "*":
+                                    adjacent += 1
+                    elif i == 0 and j != 0 and j != len(single_field[i]) - 1: # top row and middle cols
+                        if single_field[i][j - 1] == "*":
+                            adjacent += 1
+                        if single_field[i + 1][j - 1] == "*":
+                            adjacent += 1
+                        if single_field[i + 1][j] == "*":
+                            adjacent += 1
+                        if single_field[i + 1][j + 1] == "*":
+                            adjacent += 1
+                        if single_field[i][j + 1] == "*":
+                            adjacent += 1
+                    elif i == 0 and j == len(single_field[i]) - 1:  # top row and last col
+                        if single_field[i][j - 1] == "*":
+                            adjacent += 1
+                        if single_field[i + 1][j - 1] == "*":
+                            adjacent += 1
+                        if single_field[i + 1][j] == "*":
+                            adjacent += 1
+                    elif j == len(single_field[i]) - 1 and i != 0 \
+                            and i != len(single_field) - 1:  # right col and middle rows
+                        if single_field[i - 1][j] == "*":
+                            adjacent += 1
+                        if single_field[i - 1][j - 1] == "*":
+                            adjacent += 1
+                        if single_field[i][j - 1] == "*":
+                            adjacent += 1
+                        if single_field[i + 1][j - 1] == "*":
+                            adjacent += 1
+                        if single_field[i + 1][j] == "*":
+                            adjacent += 1
+                    elif i == len(single_field) - 1 and j == len(single_field[i]) - 1:  # bottom right corner
+                        if single_field[i - 1][j] == "*":
+                            adjacent += 1
+                        if single_field[i][j - 1] == "*":
+                            adjacent += 1
+                        if single_field[i - 1][j - 1] == "*":
+                            adjacent += 1
+                    elif i != 0 and i != len(single_field) - 1 and j == 0:  # left col and middle rows
+                        if single_field[i + 1][j] == "*":
+                            adjacent += 1
+                        if single_field[i + 1][j + 1] == "*":
+                            adjacent += 1
+                        if single_field[i][j + 1] == "*":
+                            adjacent += 1
+                        if single_field[i - 1][j + 1] == "*":
+                            adjacent += 1
+                        if single_field[i - 1][j] == "*":
+                            adjacent += 1
+                    elif j == 0 and i == len(single_field) - 1:  # bottom left corner
+                        if single_field[i - 1][j] == "*":
+                            adjacent += 1
+                        if single_field[i - 1][j + 1] == "*":
+                            adjacent += 1
+                        if single_field[i][j + 1] == "*":
+                            adjacent += 1
+                    elif i == len(single_field) - 1 and j != len(single_field[i]) - 1 \
+                            and j != 0:  # bottom row and middle cols
+                        if single_field[i - 1][j] == "*":
+                            adjacent += 1
+                        if single_field[i - 1][j - 1] == "*":
+                            adjacent += 1
+                        if single_field[i][j + 1] == "*":
+                            adjacent += 1
+                        if single_field[i - 1][j + 1] == "*":
+                            adjacent += 1
+                        if single_field[i][j - 1] == "*":
+                            adjacent += 1
+                    elif i != len(single_field) - 1 and j != len(single_field[i]) - 1 \
+                            and j != 0 and i != 0:  # middle rows and middle cols
+                        if single_field[i - 1][j] == "*":
+                            adjacent += 1
+                        if single_field[i - 1][j - 1] == "*":
+                            adjacent += 1
+                        if single_field[i][j + 1] == "*":
+                            adjacent += 1
+                        if single_field[i - 1][j + 1] == "*":
+                            adjacent += 1
+                        if single_field[i][j - 1] == "*":
+                            adjacent += 1
+                        if single_field[i + 1][j - 1] == "*":
+                            adjacent += 1
+                        if single_field[i + 1][j] == "*":
+                            adjacent += 1
+                        if single_field[i + 1][j + 1] == "*":
+                            adjacent += 1
+                single_field[i][j] = adjacent
+        for row in single_field:
+            for char in row:
+                self.output += str(char)
+            self.output += "\n"
+        self.output += "\n"
+
+
+if __name__ == '__main__':
+    mine = MineSweeper('mines.txt', 'minesweeper_output.txt')
+    mine.read_input()
+    print(mine.output)
+    mine.write_output()
+
+
+
+
